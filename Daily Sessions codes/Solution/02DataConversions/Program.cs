3646bb83f47181ca50269ff220af87b68a4ba843
PADDA-YOGESHWAR <103803086+PADDA-YOGESHWAR@users.noreply.github.com>
2024-06-05 12:19:57 +0530

    commit03: test commit

---
+++ Daily Sessions codes/Solution/02DataConversions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02DataConversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
